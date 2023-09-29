6ac26df2fee96c6fe64cc53d00277dd1926e0551
XiaoDree <naulialdreina@gmail.com>
2023-09-29 13:54:23 +0700

    add codeigniter 4 file

---
+++ app/Views/pages/halaman.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Perkenalkan</h1>
+    <p>Nama saya Nauli Aldre saya tinggal di daerah Solo olahraga yang saya sukai adalah futsal</p>
+</body>
+</html>
